15052ea6bc06e1604e92e7bae23b61bc3fc2cb01
Corbin Hall <hallcorbin92@gmail.com>
2020-08-18 07:39:03 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function takeANumber(line, name) {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
+}
+
+function nowServing(line) {
+  if (line.length > 0) {
+    return 'Currently serving ' + line.shift() + '.' ;
+  }
+  else {
+    return 'There is nobody waiting to be served!'
+  }
+}
+function currentLine() {
+  if (line.length > 0) {
+
+  }
+  else {
+    return 'The line is currently empty.'
+  }
+}
