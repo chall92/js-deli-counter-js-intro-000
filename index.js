54b8f6ce8c5d7bddfa118581eefe9349e5c08abd
Corbin Hall <hallcorbin92@gmail.com>
2020-08-18 08:14:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,10 @@ function currentLine(line) {
     for(let i = 0; i < line.length; i++) {
       output += (i + 1) + '. ' + line[i] + ', ' ;
     }
-    output = output.slice(0, -2) ;
+    return `The line is currently: ${output}`
   } else {
     return 'The line is currently empty.'
   }
 }
+
+  }
